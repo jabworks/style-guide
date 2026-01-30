9ed375b4f15927d4836d27c8f213e78a423dacd9
Hieu Vi <hieu1871998@gmail.com>
2026-01-30 15:41:14 -0300

    chore: tweak prettier overrides
    Signed-off-by: Hieu Vi <hieu1871998@gmail.com>

--- packages/prettier-config/src/index.ts
+++ packages/prettier-config/src/index.ts
@@ -1,27 +1,33 @@
 import { type Config } from 'prettier';
 
 export const config: Config = {
-	arrowParens: 'avoid',
-	bracketSameLine: false,
-	bracketSpacing: true,
-	endOfLine: 'lf',
-	jsxSingleQuote: true,
-	printWidth: 120,
-	quoteProps: 'as-needed',
-	semi: true,
-	singleAttributePerLine: true,
-	singleQuote: true,
-	tabWidth: 2,
-	trailingComma: 'all',
-	useTabs: false,
-	plugins: ['prettier-plugin-tailwindcss', 'prettier-plugin-sort-json', 'prettier-plugin-packagejson'],
-	overrides: [
-		{
-			files: ['*.json', '**/*.json'],
-			excludeFiles: ['package.json', '**/package.json'],
-			options: {
-				plugins: ['prettier-plugin-sort-json'],
-			},
-		},
-	],
+  arrowParens: 'avoid',
+  bracketSameLine: false,
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  jsxSingleQuote: true,
+  printWidth: 120,
+  quoteProps: 'as-needed',
+  semi: true,
+  singleAttributePerLine: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
+  plugins: ['prettier-plugin-tailwindcss', 'prettier-plugin-sort-json', 'prettier-plugin-packagejson'],
+  overrides: [
+    {
+      files: ['package.json', '**/package.json'],
+      options: {
+        plugins: ['prettier-plugin-packagejson'],
+      },
+    },
+    {
+      files: ['**/*.json'],
+      excludeFiles: ['package.json', '**/package.json'],
+      options: {
+        plugins: ['prettier-plugin-sort-json'],
+      },
+    },
+  ],
 };
