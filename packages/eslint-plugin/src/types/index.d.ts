6cedf955261b5bb45ab14513e80955c97931a0a6
Hieu Vi <hieu1871998@gmail.com>
2026-06-17 20:42:28 +0000

    fix: add node and library to eslint-plugin type declaration

--- packages/eslint-plugin/src/types/index.d.ts
+++ packages/eslint-plugin/src/types/index.d.ts
@@ -9,7 +9,9 @@ declare const plugin: {
 	readonly configs: {
 		readonly base: Readonly<Linter.Config[]>;
 		readonly comments: Readonly<Linter.Config[]>;
+		readonly library: Readonly<Linter.Config[]>;
 		readonly next: Readonly<Linter.Config[]>;
+		readonly node: Readonly<Linter.Config[]>;
 		readonly react: Readonly<Linter.Config[]>;
 		readonly typescript: Readonly<Linter.Config[]>;
 		readonly vitest: Readonly<Linter.Config[]>;
